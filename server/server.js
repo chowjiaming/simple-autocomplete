d84bd2ad56a33afe27935b024a1260f8e5c1f0c0
Joefs <joefstack@gmail.com>
2020-08-10 20:56:46 -0400

    restructured project and added dockerfiles for client and server

--- server/server.js
+++ server/server.js
@@ -12,5 +12,5 @@ app.get("*", (req, res) => {
 });
 
 // Set server listening port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
